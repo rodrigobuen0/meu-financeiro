9e03c7218d2530b5076b00009d5d0cb54052757a
Rodrigo Bueno <rodrigo.virgoo@hotmail.com>
2022-10-26 19:46:51 -0300

    Donut Chart

--- src/app/principal/principal.page.ts
+++ src/app/principal/principal.page.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as c3 from 'c3';
 
 @Component({
   selector: 'app-principal',
   templateUrl: 'principal.page.html',
   styleUrls: ['principal.page.scss']
 })
-export class PrincipalPage {
+export class PrincipalPage implements AfterViewInit{
 
-  constructor() {}
+  constructor() {
 
+}
+
+
+  ngAfterViewInit() {
+    c3.generate({
+    bindto: '#chart',
+        data: {
+            columns: [
+                ['data1', 30],
+                ['data2', 50]
+            ],
+            type : 'donut',
+        },
+        legend: {
+            position: 'right'
+        },tooltip: {
+          show: false
+      }
+    });
+}
 }
