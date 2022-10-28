2cd6cf5772430385b89f82d6c346a9c1fa12cb4c
Rodrigo Bueno <rodrigo.virgoo@hotmail.com>
2022-10-27 21:07:39 -0300

    Ajustes chart

--- src/app/principal/principal.page.ts
+++ src/app/principal/principal.page.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component } from '@angular/core';
 import * as c3 from 'c3';
+import * as d3 from 'd3';
 
 @Component({
   selector: 'app-principal',
@@ -14,20 +15,64 @@ export class PrincipalPage implements AfterViewInit{
 
 
   ngAfterViewInit() {
-    c3.generate({
+   // eslint-disable-next-line prefer-const
+   let chart = c3.generate({
     bindto: '#chart',
+    padding: {
+      top: 5,
+      right: 50,
+      bottom: 5,
+      left: 5,
+  },
         data: {
             columns: [
                 ['data1', 30],
                 ['data2', 50]
             ],
             type : 'donut',
+        },interaction: {
+          enabled: false
         },
         legend: {
-            position: 'right'
-        },tooltip: {
+          show: false,
+            position: 'right',
+            padding: 15,
+            item: {
+              tile: {
+                width: 10,
+                height: 10,
+              }
+            }
+        },
+        size: {
+          height: 120
+        },
+        tooltip: {
           show: false
+      },donut: {
+        label: {
+          show: false,
+        },
+        width: 20
       }
     });
-}
+    d3.select('.legendaChart').insert('div', '.legend').attr('class', 'legend')  .insert('ul').attr('class', 'list-group')
+    .selectAll('span')
+    .data(['data1', 'data2'])
+    .enter().append('li').attr('class', '')
+    .insert('span').attr('class', 'dot')
+    .each(function(id) {
+      d3.select(this).style('background-color', chart.color(id));
+    })
+    .insert('div').attr('class', 'legend-label')
+    // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+    .attr('data-id', function(id) { return id; })
+    .insert('span').attr('class', 'legend-label')
+  // .each(function(id) {
+  //   d3.select(this).style('background-color', chart.color(id));
+  // })
+    // eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+    .html(function(id) {return id; });
+
+  }
 }
