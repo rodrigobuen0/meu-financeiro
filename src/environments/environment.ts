d037a96ed3329c4b257ba36acb133ac9fe17537b
Rodrigo Bueno <rodrigo.virgoo@hotmail.com>
2022-12-01 21:23:51 -0300

    Ajuste telefone pag cadastro

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   // eslint-disable-next-line @typescript-eslint/naming-convention
-  //api_url: 'https://localhost:7177'
-  api_url: 'https://meu-financeiro.rodrigobueno.dev'
+  api_url: 'https://localhost:7177'
+  //api_url: 'https://meu-financeiro.rodrigobueno.dev'
 };
 
 /*
